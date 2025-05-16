862be129ee6cf3058d87e2bc6f4623424bda51e9
KaluarachchiVI <128155974+IT22054418@users.noreply.github.com>
2025-05-16 21:29:41 +0530

    GOD PLEASE PLEASE BRO IM PRAYING ON MY KNEES FOR TS

--- src/test/java/testautomation/TestCases.java
+++ src/test/java/testautomation/TestCases.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.time.Duration;
 
@@ -20,16 +20,11 @@ public class TestCases {
 
     @BeforeEach
     public void setup() {
-        System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
-
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-gpu");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--remote-allow-origins=*");
-
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // <-- this line was missing
     }
 
     @AfterEach
